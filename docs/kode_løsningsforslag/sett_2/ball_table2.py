ac8e35babed38a57213cee957fdad6f360c90893
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2020-08-22 01:23:09 +0200

    cleaned up sett_1, and fixed ball_table2.py in sett_2.

--- docs/kode_løsningsforslag/sett_2/ball_table2.py
+++ docs/kode_løsningsforslag/sett_2/ball_table2.py
@@ -1,45 +1,49 @@
-# Exercise 2.9, author Anders P. Åsbø.
-# I used the 2.8 example solution, from the 1st of september lecture, as a reference when writing the while-loop.
-n = 10  # defining the upper limit of the while-loop.
-i = 0  # starting point of the while-loop.
+"""
+Oppgave 2.9, ball_table.py, av Anders P. Åsbø.
 
-v0 = 4.0  # initial speed of the ball in m/s.
-g = 1.62  # gravitational acceleration of the moon in (m/s)/s (for fun).
+Dette programmet beregner beregner en balls posijon per tid, når den blir sparket rett opp på månen.
+Verdiene printes ut i en tabell.
+"""
+import numpy as np
 
-ts = 2.0*v0/g  # final point in time of the ball's lunar adventure.
-dt = ts/n  # defining the uniformly-spaced interval for t, such that when i == n, t == ts.
+n = 10  # antall tidssteg.
+i = 0  # initial indeks.
 
-T = []  # empty list for the time-coordinates.
-Y = []  # empty list for the space-coordinates.
+v0 = 4.0  # ballens startfart [m/s].
+g = 1.62  # månens tyngdeaksellerasjon [m/s**2].
 
-# converted the lecturer's for-loop (2.8 a) to a while-loop (2.8 b) before modifying it for 2.9.
+ts = 2.0*v0/g  # sluttverdi for tid [s].
+dt = ts/n  # beregner størelsen på tidssteg [s], slik at t == ts når i == n.
+
+T = np.empty(n+1)  # tomt array for tidskoordinater.
+Y = np.empty(n+1)  # tomt array for y-koordinater.
+
+# løper gjennom indekser:
 while i < n + 1:
-    t = i*dt  # calculating the time-coordinate,
-    T.append(t)  # and adding it to T.
-    y = v0*t - (1.0/2.0)*g*(t**2)  # calculating the space-coordinate,
-    Y.append(y)  # and adding it to Y.
-    i = i + 1  # counting i up by one.
+    T[i] = i*dt  # beregner tid siden start, og lagrer i array.
+    Y[i] = v0*T[i] - (1.0/2.0)*g*(T[i]**2)  # beregner høyde, og lagrer i array.
+    i += 1  # øker indeks med 1.
 
-# just adding the headers for my table.
-print("%s | %8s" % ("Height in meters:", "Time in seconds:"))
-# here's the exercise's for-loop using zip() to run through both lists in parallel.
+# lager overskrifter for tabell:
+print(f"Høyde i meter: | Tid i sekunder:")
+# for-løkke løper gjennom begge arrayene med zip():
 for t, y in zip(T, Y):
-    # printing the rest of the table (the "|" only lines up when y < 10).
-    print(" y = %.3fm %7s  t = %.3fs." % (y, "|", t))
+    # printer koordinater til tabell:
+    print(f" {y = :.3f} m {'':1s} | {t = :.3f} s.")
 
 # running example:
 """
-$ python3 ball_table2.py
-Height in meters: | Time in seconds:
- y = 0.000m       |  t = 0.000s.
- y = 1.778m       |  t = 0.494s.
- y = 3.160m       |  t = 0.988s.
- y = 4.148m       |  t = 1.481s.
- y = 4.741m       |  t = 1.975s.
- y = 4.938m       |  t = 2.469s.
- y = 4.741m       |  t = 2.963s.
- y = 4.148m       |  t = 3.457s.
- y = 3.160m       |  t = 3.951s.
- y = 1.778m       |  t = 4.444s.
- y = 0.000m       |  t = 4.938s.
+run ball_table2.py
+Høyde i meter: | Tid i sekunder:
+ y = 0.000 m   | t = 0.000 s.
+ y = 1.778 m   | t = 0.494 s.
+ y = 3.160 m   | t = 0.988 s.
+ y = 4.148 m   | t = 1.481 s.
+ y = 4.741 m   | t = 1.975 s.
+ y = 4.938 m   | t = 2.469 s.
+ y = 4.741 m   | t = 2.963 s.
+ y = 4.148 m   | t = 3.457 s.
+ y = 3.160 m   | t = 3.951 s.
+ y = 1.778 m   | t = 4.444 s.
+ y = 0.000 m   | t = 4.938 s.
 """
