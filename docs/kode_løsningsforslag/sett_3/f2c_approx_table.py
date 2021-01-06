6e62b4609552f7eb77fd8860c0ae4a7618da6c3c
Anders Petershagen Åsbø <anderpaa@uio.no>
2021-01-06 20:16:43 +0100

    Finskrevet f2c_approx_table.py og roots_quadratic.py

--- docs/kode_løsningsforslag/sett_3/f2c_approx_table.py
+++ docs/kode_løsningsforslag/sett_3/f2c_approx_table.py
@@ -1,28 +1,41 @@
-#Exercise 2.2, Anders P. Åsbø.
-F = 0 #defining the startingpoint.
-dF = 10 #defining the interval.
+# Exercise 2.2, Anders P. Åsbø.
 
-print(" F:   |   C:  | C_approx:") #adding header.
+F = 0  # startpunkt for temperatur [°F].
+dF = 10  # temperaturendring per iterasjon [°F].
 
-while F <= 100: #generating the table.
-    C = (5.0/9.0)*(F - 32) #calculating C.
-    C_a = (F - 30)/2.0 #approximating C.
-    print("%.2f | %.2f | %.2f" % (F,C,C_a)) #printing the table.
-    F = F + dF #counting up by one.
+print("  F:   |   C:   |   C_approx:")  # printer header.
 
-#running example:
+while F <= 100:  # løper gjennom F i [0°F, 100°F]
+    C = (5.0/9.0)*(F - 32)  # Konverterer til °C.
+    C_a = (F - 30)/2.0  # Tilnærmer temperatur i °C.
+
+    # printer verdiene:
+    print(f"{F:6.2f} | {C:6.2f} | {C_a:6.2f}")
+    """
+    Her bruker vi f-string formatering til å pynte på utskriften.
+    Ta f. eks. {F:6.2f} - 'F' er variabelen vi skal printe,
+    ':' separerer det som skal printes fra formateringsinformasjonen,
+    '6' er antall plasser utskriften skal ta opp (en plass tilsvarer ett tegn),
+    '.2' forteller at vi skal ha to gjeldene siffere etter desimalpunktet, og
+    'f' forteller at vi skal printe verdien til 'F' som flyttall (vanlig
+    desimaltall).
+    """
+
+    F += dF  # dette er det samme som F = F + dF.
+
+# kjøreeksempel:
 """
-$ python3 f2c_approx_table.py
- F:   |   C:  | C_approx:
-0.00 | -17.78 | -15.00
-10.00 | -12.22 | -10.00
-20.00 | -6.67 | -5.00
-30.00 | -1.11 | 0.00
-40.00 | 4.44 | 5.00
-50.00 | 10.00 | 10.00
-60.00 | 15.56 | 15.00
-70.00 | 21.11 | 20.00
-80.00 | 26.67 | 25.00
-90.00 | 32.22 | 30.00
-100.00 | 37.78 | 35.00
+In [2]: run f2c_approx_table.py
+  F:   |   C:   |   C_approx:
+  0.00 | -17.78 | -15.00
+ 10.00 | -12.22 | -10.00
+ 20.00 |  -6.67 |  -5.00
+ 30.00 |  -1.11 |   0.00
+ 40.00 |   4.44 |   5.00
+ 50.00 |  10.00 |  10.00
+ 60.00 |  15.56 |  15.00
+ 70.00 |  21.11 |  20.00
+ 80.00 |  26.67 |  25.00
+ 90.00 |  32.22 |  30.00
+100.00 |  37.78 |  35.00
 """
--- docs/kode_løsningsforslag/sett_3/roots_quadratic.py
+++ docs/kode_løsningsforslag/sett_3/roots_quadratic.py
@@ -1,34 +1,55 @@
-#Exercise 3.8, Anders P. Åsbø.
-#a)
-from numpy.lib.scimath import sqrt #importing the sqrt function from numpy.
+# Exercise 3.8, Anders P. Åsbø.
 
-def R(a,b,c): #defining my function.
-    R1 = ((-b) + (sqrt((b**2)-4*a*c)))/(2*a) #using the quadratic formula.
-    R2 = ((-b) - (sqrt((b**2)-4*a*c)))/(2*a)
-    return (R1,R2) #returning the roots as a tuple (or point in the complex 4D space).
+# a)
+import numpy as np  # importerer kvadratrot fra NumPy biblioteket.
+# grunnen til at jeg ikke bruker 'from math import sqrt', er fordi
+# math.sqrt ikke takler negative tall.
 
-#b)
-R2 = R(1,2,1) #testing for real roots with a known example.
-print("b) \n Solving x² + 2x + 1. \n It should be x = -1 for both roots. \n My function returns:")
-print(R2)
+
+def R(a, b, c):
+    """Returnerer røttene til et andregradspolynom.
+
+    Argumenter:
+    a -- float: andregradskoeffisienten.
+    b -- float: førstegradskoeffisienten.
+    c -- float: konstantleddet.
+    """
+    q = b**2 - 4*a*c
+
+    # hvis q < 0, så endrer jeg datatypen dens til komplekst tall,
+    # slik at numpy.sqrt ikke gir error:
+    if q < 0:
+        q = complex(q)
+
+    # regner ut røttene med andregradsformelen.
+    R1 = (-b + np.sqrt(q))/(2*a)
+    R2 = (-b - np.sqrt(q))/(2*a)
+    return R1, R2  # returnerer røttene i en tuppel.
+
+
+# b)
+R1_R2 = R(1, 2, 1)  # sjekker reelle røtter.
+print("b)", "Solving x² + 2x + 1.", "It should be x = -1 for both roots.",
+      "My function returns:", R1_R2, sep='\n')
 
 print(" ")
 
-R3 = R(1,2,2) #testing for complex roots with a known example.
-print("Solving x² + 2x + 2. \n The roots should be x1 = (-1 + j) and x2 = (-1 - j). \n My function returns:")
-print(R3)
+R1_R2 = R(1, 2, 2)  # sjekker komplekse røtter, leg merke til at j = sqrt(-1).
+print("Solving x² + 2x + 2.",
+      "The roots should be x1 = (-1 + j) and x2 = (-1 - j).",
+      "My function returns:", R1_R2, sep='\n',)
 
-#running example:
+# kjøreeksempel:
 """
-$ python3 roots_quadratic.py
+In [14]: run roots_quadratic.py
 b)
- Solving x² + 2x + 1.
- It should be x = -1 for both roots.
- My function returns:
+Solving x² + 2x + 1.
+It should be x = -1 for both roots.
+My function returns:
 (-1.0, -1.0)
 
 Solving x² + 2x + 2.
- The roots should be x1 = (-1 + j) and x2 = (-1 - j).
- My function returns:
+The roots should be x1 = (-1 + j) and x2 = (-1 - j).
+My function returns:
 ((-1+1j), (-1-1j))
 """
